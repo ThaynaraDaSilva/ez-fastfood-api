75610f6280f42779a95fdeadb4aaf02ca096a1a7
Thaynara Silva <thaynara.silva@yaman.com.br>
2024-11-15 20:36:11 -0300

    config do ngrok

--- src/main/java/br/com/fiap/ez/fastfood/frameworks/config/WebConfig.java
+++ src/main/java/br/com/fiap/ez/fastfood/frameworks/config/WebConfig.java
@@ -1,6 +1,7 @@
 package br.com.fiap.ez.fastfood.frameworks.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,17 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/swagger-ui/index.html");
+    }
+	
+	 @Override
+	public void addCorsMappings(CorsRegistry registry) {
+        // Allow all origins and methods for testing purposes
+        registry.addMapping("/**")
+        		.allowedOriginPatterns("http://9175-177-190-77-68.ngrok-free.app") // Adicione todas as URLs específicas aqui
+        		//.allowedOrigins("http://localhost:8080", "https://9175-177-190-77-68.ngrok-free.app", "http://9175-177-190-77-68.ngrok-free.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+
+                .allowedHeaders("Authorization", "Content-Type", "*")
+                .allowCredentials(true);
     }
 }
