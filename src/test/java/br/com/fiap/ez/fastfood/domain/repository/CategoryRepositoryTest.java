dddec4d69c54ed4f17d112e186bdedccad82e995
tchfer <tchfernando@gmail.com>
2024-12-10 01:13:05 -0300

    Criar domain repository com category

---
+++ src/test/java/br/com/fiap/ez/fastfood/domain/repository/CategoryRepositoryTest.java
@@ -0,0 +1,14 @@
+package br.com.fiap.ez.fastfood.domain.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CategoryRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
